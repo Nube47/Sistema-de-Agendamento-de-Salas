2b5afc4843654500df4d0750c724b2080c729417
George Lucas <george.lucasdzzs@gmail.com>
2026-09-07 00:37:12 -0300

    inicio DOM

---
+++ js/dom.js
@@ -0,0 +1,5 @@
+export const dadosBlocos = {
+    "Bloco A": ["Sala 101", "Sala 102", "Lab de Informática A"],
+    "Bloco B": ["Sala 201", "Sala 202", "Lab de Química"],
+    "Bloco C": ["Auditório", "Sala de Reuniões"]
+};
